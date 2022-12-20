d31bf3e5bdbe385aeed4664003c0bfcd2f1a4d14
doddoreul <lghs@pop-os.localdomain>
2022-12-20 11:47:00 +0100

    migrating arduino project to pio

--- arduino/motor_driver/src/motor_driver.ino
+++ arduino/motor_driver/src/motor_driver.ino
@@ -1,10 +1,12 @@
+#include <Arduino.h>
 
 const int leftWheel = 11;
 const int rightWheel = 10;
 
+char readSerial;
+
 void setup() {
   Serial.begin(9600);
-
 }
 
 void loop() {
@@ -12,15 +14,23 @@ void loop() {
     readSerial = Serial.read();
     move(readSerial);
   }
-
 }
 
-void move(dir){
-  if (dir == "left"){
- 
-  } elseif (dir == "right) {
-    
+void move(char dir) {
+  if (dir == 'left'){
+    // Set the left and right motor speeds
+    analogWrite(leftWheel, 180);
+    analogWrite(rightWheel, 0);
+  } else if (dir == 'right') {
+    // Set the left and right motor speeds
+    analogWrite(leftWheel, 0);
+    analogWrite(rightWheel, 180);
+  } else if (dir == "foward") {
+    // Set the left and right motor speeds to 0
+    analogWrite(leftWheel, 180);
+    analogWrite(rightWheel, 180);
   } else {
-    
+    analogWrite(leftWheel, 0);
+    analogWrite(rightWheel, 0);
   }
 }
